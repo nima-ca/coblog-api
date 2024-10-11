85b9ee733c81d5f4ca48de38f1c5762f72707d15
Nima Nekouei Nia <nekoueinima@gmail.com>
2024-10-11 13:26:36 +0330

    feat: generate required modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,12 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import configurations from 'configs/config';
 import { getTypeORMConfig } from 'configs/db/orm.config';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
+import { CommentModule } from './comment/comment.module';
+import { CategoryModule } from './category/category.module';
+import { TagModule } from './tag/tag.module';
+import { LikeModule } from './like/like.module';
 
 @Module({
     imports: [
@@ -12,6 +18,12 @@ import { getTypeORMConfig } from 'configs/db/orm.config';
             envFilePath: './configs/envs/.development.env',
         }),
         TypeOrmModule.forRoot(getTypeORMConfig()),
+        UserModule,
+        PostModule,
+        CommentModule,
+        CategoryModule,
+        TagModule,
+        LikeModule,
     ],
     controllers: [],
     providers: [],
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
+import { VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    app.enableVersioning({ type: VersioningType.URI });
+
     const configService = app.get(ConfigService);
     const port = configService.get('PORT');
-
     await app.listen(port);
 }
+
 bootstrap();
